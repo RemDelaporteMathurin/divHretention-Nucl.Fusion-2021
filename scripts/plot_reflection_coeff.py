669d2c10c3ae45e6c416cc011d785a42f5d1a7e1
remdelaportemathurin <delaporte.mathurin@laposte.net>
2021-06-16 10:58:14 +0200

    alternate version + blues

--- scripts/plot_reflection_coeff.py
+++ scripts/plot_reflection_coeff.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+from matplotlib import cm
 import numpy as np
 from scipy.stats import linregress
 
@@ -18,31 +20,46 @@ plt.rc('font', family='serif', size=12)
 with pkg_resources.path(data_module, "data_TRIM_energy_angle.csv") as p:
     data = np.genfromtxt(p, delimiter=";", names=True)
 
-plt.figure(figsize=[6.4, 4])
 
-for angle in np.unique(data["theta_inc"]):
+plt.figure(1, figsize=[6.4, 4])
+angles = np.unique(data["theta_inc"])
+colormap = cm.Blues
+for angle in angles:
     indexes = np.where(data["theta_inc"] == angle)
 
     energy = data["Incident_energy"][indexes]
-    R_p = data["Reflection_coeff"][indexes]
+    r = data["Reflection_coeff"][indexes]
 
-    # add jitter for better visualisation
+    plt.figure(1)
+    offset = 50
+    colour = colormap((angle + offset)/(90 + offset))
     plt.scatter(
         energy,
-        R_p,
+        r,
         label="{:.0f} °".format(angle),
-        alpha=0.6)
+        alpha=0.6,
+        color=colour)
     plt.plot(
         energy,
-        [reflection_coeff(energy[i], angle) for i in range(len(energy))])
+        [reflection_coeff(energy[i], angle) for i in range(len(energy))],
+        color=colour)
+    plt.annotate("{:.0f} °".format(angle), (energy[-1] * 1.05, r[-1]), color=colour)
+    plt.figure(2)
 
+    stdev_x = .005 * (max(angles) - min(angles))
+    plt.scatter(
+        np.ones(energy.shape)*angle + np.random.randn(len(energy)) * stdev_x,
+        r,
+        c=energy,
+        cmap="viridis",
+        norm=LogNorm(),
+        alpha=0.6)
 
+plt.figure(1)
 plt.ylim(bottom=0)
 plt.xscale("log")
-# plt.yscale("log")
-plt.legend()
 plt.xlabel("Incident energy (eV)")
-plt.ylabel("Implantation range (nm)")
+plt.ylabel("Reflection coefficient")
 ax = plt.gca()
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
@@ -51,5 +68,16 @@ plt.tight_layout()
 
 plt.savefig("../Figures/reflection_coeff.pdf")
 plt.savefig("../Figures/reflection_coeff.svg")
+
+plt.figure(2)
+plt.colorbar()
+plt.ylim(bottom=0)
+plt.xlabel("Angle of incidence (°)")
+plt.ylabel("Reflection coefficient")
+ax = plt.gca()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+plt.tight_layout()
 
 plt.show()
