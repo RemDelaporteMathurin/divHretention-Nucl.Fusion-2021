f66a4aa0bf386d3136d68d76e561b4a49d120cab
remdelaportemathurin <delaporte.mathurin@laposte.net>
2021-06-21 15:33:59 +0200

    fixed a few things

--- scripts/plot_puff_rate_scan.py
+++ scripts/plot_puff_rate_scan.py
@@ -52,7 +52,7 @@ my_plot = plot_along_divertor(
 
 plt.tight_layout()
 plt.colorbar(
-    sm, label="Puff rate (s$^{-1}$)",
+    sm, label="Puffing rate (H$_2$ s$^{-1}$)",
     ax=my_plot.axs)
 
 for ax in my_plot.axs:
@@ -108,7 +108,7 @@ for i, filename in enumerate(filenames):
     ratio_ions_outer_sp.append(c_max_ions[outer_sp_loc_index]/c_max[outer_sp_loc_index])
     ratio_ions_private_zone.append(c_max_ions[private_zone_sp_loc_index]/c_max[private_zone_sp_loc_index])
 
-plt.colorbar(sm, label="Puff rate (s$^{-1}$)")
+plt.colorbar(sm, label="Puffing rate (H$_2$ s$^{-1}$)")
 plt.ylim(0, 1)
 plt.xlabel("Distance along divertor (m)")
 plt.ylabel("c surface (ions) / c surface")
@@ -128,7 +128,7 @@ plt.annotate(
     (3e21, 4.4e23),
     color=line.get_color())
 plt.scatter(Ps, integrated_inventories, marker="+")
-plt.xlabel("Puff rate (s$^{-1}$)")
+plt.xlabel("Puffing rate (H$_2$ s$^{-1}$)")
 plt.ylabel("Divertor H inventory (H)")
 plt.ylim(bottom=0)
 plt.xlim(left=0)
@@ -167,11 +167,17 @@ plt.fill_between(
     10**(-2*sigma_private_zone + np.log10(inventory_private_zone)),
     facecolor=line_pz.get_color(), alpha=0.3)
 
-plt.annotate("Inner strike point", (1.05*Ps[-1], inventory_strike_point_inner[-1]), color=line_spi.get_color())
-plt.annotate("Outer strike point", (1.05*Ps[-1], inventory_strike_point_outer[-1]), color=line_spo.get_color())
+plt.annotate(
+    "Inner strike point",
+    (1.05*Ps[-1], 0.91*inventory_strike_point_inner[-1]),
+    color=line_spi.get_color())
+plt.annotate(
+    "Outer strike point",
+    (1.05*Ps[-1], inventory_strike_point_outer[-1]),
+    color=line_spo.get_color())
 plt.annotate("Private zone", (1.05*Ps[-1], inventory_private_zone[-1]), color=line_pz.get_color())
 plt.xlim(right=1.4*Ps[-1])
-plt.xlabel("Puff rate (s$^{-1}$)")
+plt.xlabel("Puffing rate (s$^{-1}$)")
 plt.ylabel("Inventory (H/m)")
 plt.ylim(bottom=0)
 plt.xlim(left=0)
@@ -222,9 +228,7 @@ axs[2].annotate("Atoms", (1e21, 0.5), color="white", weight="bold")
 
 plt.sca(axs[0])
 plt.xlim(left=Ps[0], right=Ps[-1])
-axs[0].set_xlabel("Puff rate (s$^{-1}$)")
-axs[1].set_xlabel("Puff rate (s$^{-1}$)")
-axs[2].set_xlabel("Puff rate (s$^{-1}$)")
+axs[1].set_xlabel("Puffing rate \n (H$_2$ s$^{-1}$)")
 plt.ylabel(r"$c_{\mathrm{surface}, \mathrm{ions}} / c_\mathrm{surface}$")
 plt.ylim(bottom=0, top=1)
 plt.yticks(ticks=[0, 0.5, 1])
