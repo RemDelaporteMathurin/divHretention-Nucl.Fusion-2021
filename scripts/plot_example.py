64b0928a185ace9cb6a8deeba53f964b7e706c96
RemDelaporteMathurin <delaporte.mathurin@laposte.net>
2021-06-22 09:39:02 +0200

    added ITER example

--- scripts/plot_example.py
+++ scripts/plot_example.py
@@ -6,11 +6,30 @@ from divHretention import plot_Tc_map_with_subplots, DEFAULT_TIME, \
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif', size=12)
 
+# ITER
+filename = "../data/exposure_conditions_divertor/ITER/2398/2398_inner_target.csv"
+
+my_plot = plot_Tc_map_with_subplots(
+    filenames=[filename], filetypes="ITER",
+    T_bounds=[320, 1200], figsize=(5, 5))
+my_plot.axs_bottom[1].get_yaxis().set_ticks([])
+my_plot.axs_top[1].yaxis.set_label_position("right")
+my_plot.axs_top[1].yaxis.tick_right()
+my_plot.axs_top[1].set_ylabel("Inventory per unit \n thickness (H/m)")
+
+# change everything to orange
+for ax in [*my_plot.axs_top, *my_plot.axs_bottom]:
+    for line in ax.get_lines():
+        line.set_color("tab:orange")
+
+for cl in my_plot.axs_top[1].collections:
+    cl.set_facecolor("tab:orange")
+
+plt.tight_layout()
+
+
+# WEST
 filename = "../data/exposure_conditions_divertor/WEST/West-LSN-P2.5e+21-IP0.449MW.csv"
-
-
-# trigger DEFAULT_TIME
-# compute_inventory([300], [1e20], DEFAULT_TIME)
 
 my_plot = plot_Tc_map_with_subplots(
     filenames=[filename], filetypes="WEST",
@@ -29,4 +48,5 @@ for cl in my_plot.axs_top[1].collections:
     cl.set_facecolor("tab:orange")
 
 plt.tight_layout()
+
 my_plot.show()
